567b9905ac892fcb56d38e7b143c7ee673b67cc6
tonyomendoza <mendozatony@outlook.com>
2021-09-20 13:05:01 -0700

    Lesson 11 Connecting Mongoose

--- server.js
+++ server.js
@@ -1,6 +1,15 @@
 const express = require('express')
 const mongoose = require('mongoose')
 
+// connect to MongoDB using the ORM
+mongoose.connect('mongodb://localhost/world') // pass this as a second argument if supported: {userNewURLParser: true}
+    .then(data =>{
+        console.log('Mongo DB connection success!');
+    })
+    .catch(err =>{
+        console.log('Mongo DB connection failed:' + err.message);
+    });
+
 const app = express()
 
 app.use('/', (req, res, next) => {
